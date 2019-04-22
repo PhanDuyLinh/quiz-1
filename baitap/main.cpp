907c6333a787ff818fb0613a3dece3cda5fa1cab
PhanDuyLinh <49575284+PhanDuyLinh@users.noreply.github.com>
2019-04-23 00:53:46 +0700

    Update main.cpp

--- baitap/main.cpp
+++ baitap/main.cpp
@@ -19,12 +19,27 @@ public:
         {
             return a*a*3.14;
         }
+        static int num ;
+	
+       	 Area()
+	 {   
+	        num++ ;
+	 }
+        // Get number of objects
+	int getNumOfObject()
+	{
+               return Area::num ;
+	}
 };
-
+int Area::num = 0 ;
 int main()
 {
     cout << "Area of square with edge = 2 is: " << Area::cal(2.0);
     cout << "\nArea of rectangle with height = 2, width = 3 is :"<<Area::cal(2,3);
     cout << "\nArea of circle with radius = 2 is: "<<Area::cal(2);
+    Area object1 ;
+    Area object2 ;
+    Area object3 ;
+    cout << "Number of objects is : " << object3.getNumOfObject() <<endl ;
     return 0;
 }
